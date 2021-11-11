0512af49ed2225d3fb61d41e1a2ea9b25aff6805
minirachel <rachel.a.levinson@gmail.com>
2021-11-11 14:00:05 -0500

    commit tree ae546e59ecf7e1033f1dffa4a93ec6e150280aa7

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,4 +2,18 @@ module ArtistsHelper
   def display_artist(song)
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
+
+  def display_name(song, path)
+    if song.artist
+      song.artist.name
+    end
+  end
+
+  def artist_select(song, path)
+    if song.artist.nil?
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+    else
+      hidden_field_tag "song[artist_id]", song.artist_id
+    end
+  end
 end
